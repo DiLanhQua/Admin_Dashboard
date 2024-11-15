b6796ffbb2f997431f20d71cc125d1716508e2e1
Huy Ho <azirkingsand0.12@gmail.com>
2024-11-15 19:14:24 +0700

    fix

--- src/services/brand.service.js
+++ src/services/brand.service.js
@@ -1,7 +1,7 @@
 import sendRequest from "../utils/resquest";
 
 const BrandService = {
-  getAll: () => sendRequest("get", "/brands/"),
+  getAll: () => sendRequest("get", "/Brand/get-all-brand/"),
   create: (data) => sendRequest("post", "/brands/create", data),
   delete: (id) => sendRequest("delete", `/brands/${id}`),
   update: (id, data) => sendRequest("put", `/brands/${id}`, data),
