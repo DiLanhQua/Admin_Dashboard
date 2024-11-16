202211817e89a66ec1e0d376be4c27fb3a0d3df9
vanlap20 <lapday122@gmail.com>
2024-11-16 16:01:04 +0700

    brand

--- src/pages/brand/create/index.jsx
+++ src/pages/brand/create/index.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable react/prop-types */
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import {
   TextField,
@@ -10,125 +8,171 @@ import {
   CardContent,
   CardHeader,
 } from "@mui/material";
-import ImageUploader from "../../../components/upload";
-import Textarea from "../../../components/textarea";
-import slugify from "../../../utils/slugify";
+import * as Yup from "yup";
 
 export default function BrandForm({
   title,
-  onSubmit,
+  onAdd,
+  onUpdate,
   initialValues,
-  validationSchema,
   isUpdate,
 }) {
-  const [isSlugEdited, setIsSlugEdited] = useState(false);
+  const [imageFile, setImageFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImageFile(file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleImageDelete = () => {
+    setImageFile(null);
+    setPreview(null);
+  };
+
+  const handleSubmit = async (values) => {
+    const formData = new FormData();
+    formData.append("BrandName", values.BrandName);
+    formData.append("country", values.country);
+
+    if (imageFile) {
+      formData.append("Picture", imageFile);
+    }
+    for (let pair of formData.entries()) {
+      console.log(pair[0]+ ': ' + pair[1]);
+    }
+    console.log("image file:", imageFile);
+
+    try {
+      if (isUpdate) {
+        await onUpdate(formData);
+      } else {
+        await onAdd(formData);
+      }
+    } catch (error) {
+      console.error("Lỗi khi submit form:", error);
+    }
+  };
 
   return (
     <Formik
       initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
+      validationSchema={Yup.object().shape({
+        BrandName: Yup.string().required("Tên thương hiệu không được để trống"),
+        country: Yup.string().required("Xuất xứ không được để trống"),
+      })}
+      onSubmit={handleSubmit}
       enableReinitialize={true}
     >
-      {({ errors, touched, handleChange, values, setFieldValue }) => {
-        // Update the slug when the name changes, but only if the user hasn't manually edited the slug
-        useEffect(() => {
-          if (values.name && !isSlugEdited) {
-            const generatedSlug = slugify(values.name);
-            setFieldValue("slug", generatedSlug);
-          }
-        }, [values.name, isSlugEdited, setFieldValue]);
-
-        // Handle changes in the slug field and set the state accordingly
-        const handleSlugChange = (e) => {
-          setIsSlugEdited(true); // Mark slug as manually edited
-          setFieldValue("slug", e.target.value);
-        };
-
-        // Handle image upload and set the image URL
-        const handleImageUpload = (url) => {
-          setFieldValue("image", url);
-        };
-
-        const handleImageDelete = () => {
-          setFieldValue("image", "");
-        };
-
-        return (
-          <Card>
-            <CardHeader title={title} />
-            <CardContent>
-              <Form>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <ImageUploader
-                      label="Hình ảnh"
-                      name="image"
-                      onUploadComplete={handleImageUpload} // Custom handler for uploading images
-                      onDelete={handleImageDelete} // Custom handler for deleting images
-                      error={errors.image && touched.image}
-                      helperText={
-                        errors.image && touched.image ? errors.image : ""
-                      }
-                      fooder="brand" // Dynamic folder for image upload
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Field
-                      name="name"
-                      as={TextField}
-                      fullWidth
-                      label="Tên thương hiệu"
-                      variant="outlined"
-                      value={values.name || ""}
-                      onChange={handleChange}
-                      error={errors.name && touched.name}
-                      helperText={
-                        errors.name && touched.name ? errors.name : ""
-                      }
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <TextField
-                      fullWidth
-                      label="Slug"
-                      variant="outlined"
-                      value={values.slug || ""}
-                      onChange={handleSlugChange} // Custom handler for slug changes
-                      error={errors.slug && touched.slug}
-                      helperText={
-                        errors.slug && touched.slug ? errors.slug : ""
-                      }
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Textarea
-                      label="Mô tả"
-                      name="description"
-                      value={values.description || ""}
-                      onChange={handleChange}
-                      error={errors.description && touched.description}
-                      helperText={
-                        errors.description && touched.description
-                          ? errors.description
-                          : ""
-                      }
-                    />
-                  </Grid>
-
-                  <Grid item xs={12} container justifyContent="flex-end">
-                    <Button type="submit" variant="contained" color="success">
-                      {isUpdate ? "Cập nhật thương hiệu" : "Thêm thương hiệu"}
-                    </Button>
-                  </Grid>
+      {({ errors, touched, handleChange, values }) => (
+        <Card>
+          <CardHeader title={title} />
+          <CardContent>
+            <Form encType="multipart/form-data">
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  {/* Khung chứa ảnh */}
+                  <div
+                    style={{
+                      border: "2px dashed #ccc", // Viền khung ảnh
+                      borderRadius: "8px", // Góc bo tròn
+                      padding: "10px",
+                      textAlign: "center",
+                      width: "100%",
+                      minHeight: "200px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    {/* Chọn ảnh */}
+                    {!preview ? (
+                      <>
+                        <input
+                          name="image"
+                          type="file"
+                          accept="image/*"
+                          onChange={handleFileChange}
+                          style={{ display: "none" }}
+                          id="image-upload"
+                        />
+                        <label htmlFor="image-upload">
+                          <Button variant="contained" component="span">
+                            Chọn ảnh
+                          </Button>
+                        </label>
+                      </>
+                    ) : (
+                      <>
+                        <img
+                          src={preview}
+                          alt="Preview"
+                          style={{
+                            maxWidth: "100%",
+                            maxHeight: "200px",
+                            objectFit: "contain", // Đảm bảo ảnh không bị méo
+                          }}
+                        />
+                        <Button
+                          variant="outlined"
+                          color="secondary"
+                          onClick={handleImageDelete}
+                          style={{
+                            marginTop: "10px",
+                            marginLeft: "10px",
+                          }}
+                        >
+                          Xóa ảnh
+                        </Button>
+                      </>
+                    )}
+                  </div>
                 </Grid>
-              </Form>
-            </CardContent>
-          </Card>
-        );
-      }}
+
+                <Grid item xs={6}>
+                  <Field
+                    name="BrandName"
+                    as={TextField}
+                    fullWidth
+                    label="Tên thương hiệu"
+                    value={values.BrandName || ""}
+                    onChange={handleChange}
+                    error={errors.BrandName && touched.BrandName}
+                    helperText={errors.BrandName && touched.BrandName ? errors.BrandName : ""}
+                  />
+                </Grid>
+
+                <Grid item xs={6}>
+                  <Field
+                    name="country"
+                    as={TextField}
+                    fullWidth
+                    label="Xuất Xứ"
+                    value={values.country || ""}
+                    onChange={handleChange}
+                    error={errors.country && touched.country}
+                    helperText={errors.country && touched.country ? errors.country : ""}
+                  />
+                </Grid>
+
+                <Grid item xs={12} container justifyContent="flex-end">
+                  <Button type="submit" variant="contained" color="success">
+                    {isUpdate ? "Cập nhật thương hiệu" : "Thêm thương hiệu"}
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          </CardContent>
+        </Card>
+      )}
     </Formik>
   );
 }
