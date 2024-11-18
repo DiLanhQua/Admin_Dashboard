ff9662421c638eb6ae37ef9574b172123868ce19
tuannguyen91120033 <tuannguyen91120033@gmail.com>
2024-11-18 15:07:15 +0700

    voucher

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "/app/dashboard/analytics";
-export const BASE_TITLE = " | Voi Tây Nguyên";
+export const BASE_TITLE = " | Shop Coza";
 
 export const CONFIG = {
   layout: "vertical",
